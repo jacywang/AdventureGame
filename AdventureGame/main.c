edf3f14eea94d169e24131b2ae339a270c8390c5
Jacy Wang <jacywj@gmail.com>
2015-05-06 23:57:28 -0700

    Add enum

---
+++ AdventureGame/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  AdventureGame
+//
+//  Created by JIAN WANG on 5/6/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#include <stdio.h>
+
+typedef enum {
+    empty,
+    cube,
+    treasure,
+} RoomValue;
+
+int main(int argc, const char * argv[]) {
+
+    
+    
+    return 0;
+}
